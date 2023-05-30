a68c7c2857bdcdc5d4946f1e1546fba0ad62ac4b
VladimirAscenio <134453429+VladimirAscencio@users.noreply.github.com>
2023-05-29 20:15:51 -0600

    Update BancoElCaribe.py

--- BancoElCaribe.py
+++ BancoElCaribe.py
@@ -1,1 +1,37 @@
+print("""
+         *****************************************************************
+         *      BIENVENIDOS AL PROGRAMA DE EL BANCO EL CARIBE            *
+         *****************************************************************""")
 
+print("\nA CONTINUACIÓN SE TE PRESENTA EL MENÚ DE OPCIONES DE NUESTRO PROGRAMA: ")
+
+respuesta = int(input("\n 1. Agregar cliente. \n 2. Modificar cliente. \n 3. Agregar transacción. \n 4. Eliminar cliente. \n 5. Mostrar lista de clientes con depósitos. \n 6. Mostrar lista de clientes con retiros. \n 7. Mostrar lista con clientes ordenada por Número de cuenta. \n 8. Salir del programa. \n Agrega la opción del menú: "))
+
+while i:
+    if respuesta == "1":
+        print("D")
+    elif respuesta == "2":
+        print("D")
+    elif respuesta == "3":
+        print("C")
+    elif respuesta == "4":
+        print("C")
+    elif respuesta == "5":
+        print("G")
+    elif respuesta == "6":
+        print("G")
+    elif respuesta == "7":
+        print("V")
+    elif respuesta == "8":
+        print("""\n
+                  **************************************
+                  *        EL BANCO EL CARIBE          *
+                  **************************************""")
+        print("""\n
+                ********************************************
+                *    HA FINALIZADO EL PROGRAMA             *
+                ********************************************""")
+        break
+    else:
+        print("Ha ingresado una opción incorrecta. Por favor ingresa una opción del menú.")
+        continue 
