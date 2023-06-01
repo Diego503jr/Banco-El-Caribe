cb4685b4e46bfb9f8ed8d3e66f73dc33a6ec5b34
Carias <diegocarias503@gmail.com>
2023-06-01 10:26:12 -0600

    Si no hay clientes muestra que no hay y lo saca del ciclo al menu

--- BancoElCaribe.py
+++ BancoElCaribe.py
@@ -1,18 +1,20 @@
-#Datos quemados son esos y se pueden revisar descomentando lo que esta comentado y vicerversa con lo que no esta comentado en la opcion 
+#Lista para eliminar el cliente de una vez //se supone que aquí se añadira el diccionario cliente xd
+listaClientes = []
 clientes = {
     "codigoClientes" : [12345],
     "nombreClientes" : ["Diego"],
-    "numeroCuenta" : [123567],
-    "retiros" : [100],
-    "depositos" : [100],
-    "fechaRe" : ["12/21/31"],
-    "fechaDe" : ["12/21/31"],
-    "total" : [200]
+    "numeroCuenta" : [123456],
+    "retiros" : [2],
+    "depositos" : [3],
+    "fechaRe" : ["12/03/23"],
+    "fechaDe" : ["12/03/23"],
+    "total" : [1233]
 }
 totDep = 0
 totRet = 0
 cantClientes = 1
 
+#Esta funcion lo que hace es manejar el error si se escribe Strings muestra el error que no es un valor numerico
 def manejoErrorOpcion(msj):
     while True:
         try:
@@ -57,57 +59,59 @@ while True:
     elif respuesta == 2:
         print("\n --- AGREGAR TRANSACCION --- ")
         while True:
-            #Esta es la funcion "manejoErrorOpcion" para manejar errores tipo ValueError y pregunta si desea continuar con el proceso
             respuesta = manejoErrorOpcion("\nEscoja una opcion \n1.Seguir \n2.Regresar:\nIngrese la opción deseada: ")
             if respuesta == 1:
-                codigo = manejoErrorOpcion("\nDigite el código de cliente: ")
-                for i in range(cantClientes):
-                    #Verifica si hay un codigo que concuerde con el cliente
-                    if clientes["codigoClientes"][i] == codigo:
-                        print("\n   --- Cliente Encontrado! ---")
-                        print("Codigo Cliente: \t", clientes["codigoClientes"][i])
-                        print("Nombre Cliente: \t", clientes["nombreClientes"][i])
-                        print("Cuenta N°: \t\t", clientes["numeroCuenta"][i])
-                        #Aun no sirve el agregar porque esta complejo esto xd
-                        tipoTransaccionAgregar = manejoErrorOpcion("\n\nTipo de transacción que desea realizar:\n1- Deposito\n2- Retiro\n3- Regresar al submenú\nIngrese la opción deseada: ")
-                        if tipoTransaccionAgregar == 1 :
+                #Verifica si hay clientes sino hay muestra un mensaje que no hay y lo saca porque sino hay que mas tiene que hacer aqui xd
+                if cantClientes > 0:
+                    codigo = manejoErrorOpcion("\nDigite el código de cliente: ")
+                    for i in range(cantClientes):
+                        #Verifica si hay un codigo que concuerde con el cliente
+                        if clientes["codigoClientes"][i] == codigo:
+                            print("\n   --- Cliente Encontrado! ---")
+                            print("Codigo Cliente: \t", clientes["codigoClientes"][i])
+                            print("Nombre Cliente: \t", clientes["nombreClientes"][i])
+                            print("Cuenta N°: \t\t", clientes["numeroCuenta"][i])
+                            #Aun no sirve el agregar porque esta complejo esto xd
+                            tipoTransaccionAgregar = manejoErrorOpcion("\n\nTipo de transacción que desea realizar:\n1- Deposito\n2- Retiro\n3- Regresar al submenú\nIngrese la opción deseada: ")
+                            if tipoTransaccionAgregar == 1 :
 
-                            print("\n - DEPOSITO - ")
-                            valorDeposito = manejoErrorOpcion("Ingrese la cifra a depositar: $ ")
-                            clientes["depositos"].append(valorDeposito)
-                            totDep += valorDeposito
-                            fechaDeposito = manejoErrorOpcion("Digite la fecha de este depósito: ")
-                            clientes["depositos"].append(valorDeposito)
-                            clientes["fechaDe"].append(fechaDeposito)
+                                print("\n - DEPOSITO - ")
+                                valorDeposito = manejoErrorOpcion("Ingrese la cifra a depositar: $ ")
+                                clientes["depositos"].append(valorDeposito)
+                                totDep += valorDeposito
+                                fechaDeposito = input("Digite la fecha de este depósito: ")
+                                clientes["depositos"].append(valorDeposito)
+                                clientes["fechaDe"].append(fechaDeposito)
 
-                            tipoTransaccionAgregar = manejoErrorOpcion("\n3- Regresar al submenu\nIngrese la opción: ")
+                                tipoTransaccionAgregar = manejoErrorOpcion("\n3- Regresar al submenu\nIngrese la opción: ")
 
-                        elif tipoTransaccionAgregar == 2:
+                            elif tipoTransaccionAgregar == 2:
 
-                            print("\n - RETIRO -")
-                            valorRetiro = manejoErrorOpcion("\nIngrese la cifra a depositar: $ ")
-                            clientes["retiros"].append(valorRetiro)
-                            totRet += valorRetiro
-                            fechaRetiro = input("Digite la fecha de este retiro: ")
-                            clientes["retiros"].append(valorRetiro)
-                            clientes["fechaRe"].append(fechaRetiro)
+                                print("\n - RETIRO -")
+                                valorRetiro = manejoErrorOpcion("\nIngrese la cifra a depositar: $ ")
+                                clientes["retiros"].append(valorRetiro)
+                                totRet += valorRetiro
+                                fechaRetiro = input("Digite la fecha de este retiro: ")
+                                clientes["retiros"].append(valorRetiro)
+                                clientes["fechaRe"].append(fechaRetiro)
 
-                            tipoTransaccionAgregar = manejoErrorOpcion("\n3- Regresar al submenu\nIngrese la opción: ")
+                                tipoTransaccionAgregar = manejoErrorOpcion("\n3- Regresar al submenu\nIngrese la opción: ")
 
-                        elif tipoTransaccionAgregar == 3:
-                            break
+                            elif tipoTransaccionAgregar == 3:
+                                break
+                            else:
+                                print("\nPor favor ingresa una de las opciones dadas!")
                         else:
-                            print("\nPor favor ingresa una de las opciones dadas!")
-                            continue
-                    else:
-                        print("\n   --- Cliente No Encontrado! ---")
-                saldoF = totDep -totRet
+                            print("\n   --- Cliente No Encontrado! ---")
+                    saldoF = totDep -totRet
+                else:
+                    print("\n   --- No hay clientes ---")  
+                    break
             elif respuesta == 2:
                 break
             else:
                 print("\nHa ingresado una opción incorrecta. Por favor ingresa una opción del submenú.")
-
-
+                
     elif respuesta == 3: #En este si el cliente no tiene retiros o depositos, en esa parte da error :c
             codigo = input("\nDigite el código de cliente: ")
             # for i in range(cantClientes):
@@ -149,27 +153,33 @@ while True:
         while True:
             respuesta = manejoErrorOpcion("\nEscoja una opcion \n1.Seguir \n2.Regresar:\nIngrese la opción deseada: ")
             if respuesta == 1:
-                codigo = manejoErrorOpcion("\nDigite el código de cliente: ")
-                for i in range(cantClientes):
-                    print("\n   --- Cliente Encontrado! ---")
-                    print("Codigo Cliente: \t", clientes["codigoClientes"][i])
-                    print("Nombre Cliente: \t", clientes["nombreClientes"][i])
-                    print("Cuenta N°: \t\t", clientes["numeroCuenta"][i])
-                    #Aqui aun no se elimina solo esta como demostracion
-                    if clientes["codigoClientes"][i] == codigo:
-                        confirmation = input("\n¿Deseas eliminar el cliente? (si/no): ")
-                        if confirmation.lower() == "si":
-                            clientes["codigoClientes"].remove(codigoClientes)
-                            clientes["nombreClientes"].remove(nombreClientes)
-                            clientes["numeroCuenta"].remove(numeroCuenta)
-                            break
-                        elif confirmation.lower() == "no":
-                            print("\nNo se elimino ningun cliente")
-                            break
+                #Verifica si hay clientes sino hay muestra un mensaje que no hay y lo saca porque sino hay que mas tiene que hacer aqui xd
+                if cantClientes > 0:
+                    codigo = manejoErrorOpcion("\nDigite el código de cliente: ")
+                    for i in range(cantClientes):
+                        print("\n   --- Cliente Encontrado! ---")
+                        print("\nCodigo Cliente: \t", clientes["codigoClientes"][i])
+                        print("Nombre Cliente: \t", clientes["nombreClientes"][i])
+                        print("Cuenta N°: \t\t", clientes["numeroCuenta"][i])
+                        #Aqui aun no se elimina solo esta como demostracion esta complejo el bolado xd
+                        if clientes["codigoClientes"][i] == codigo:
+                            confirmation = input("\n¿Deseas eliminar el cliente? (si/no): ")
+                            if confirmation.lower() == "si":
+                                # clientes["codigoClientes"].remove(codigoClientes)
+                                # clientes["nombreClientes"].remove(nombreClientes)
+                                # clientes["numeroCuenta"].remove(numeroCuenta)
+                                print("\n   --- Se borro el cliente ---")
+                                break
+                            elif confirmation.lower() == "no":
+                                print("\n   --- No se borro el cliente ---")
+                                break
+                            else:
+                                print("\nHa ingresado una opción incorrecta. Por favor ingresa una opción del submenú.")
                         else:
-                            print("\nHa ingresado una opción incorrecta. Por favor ingresa una opción del submenú.")
-                    else:
-                        print("\n   --- Cliente No Encontrado! ---")
+                            print("\n   --- Cliente No Encontrado! ---")
+                else:
+                    print("\n   --- No hay clientes ---")
+                    break
             elif respuesta == 2:
                 break
             else:
