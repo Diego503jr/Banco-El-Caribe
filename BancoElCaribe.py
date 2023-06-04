4fe148af006cafa61aff5929b7e2c580813b2292
VladimirAscenio <134453429+VladimirAscencio@users.noreply.github.com>
2023-06-04 11:08:19 -0600

    Update BancoElCaribe.py

--- BancoElCaribe.py
+++ BancoElCaribe.py
@@ -1,8 +1,8 @@
 import time
 print(" CALCULANDO", end= " ")
- for t in range(1):
-      print("...", end= " ")
-      time.sleep(4)
+ for t in range(5):
+      print(" . ", end= " ")
+      time.sleep(5)
 #Datos quemados son esos y se pueden revisar descomentando lo que esta comentado y vicerversa con lo que no esta comentado en la opcion 
 clientes = {
     "codigoClientes" : [1001,1002,1003,1004],
